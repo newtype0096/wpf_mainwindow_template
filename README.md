c2a6c0d57c63182163fd90612306045e471fd001
newtype0096 <newtype0096@naver.com>
2024-07-08 10:03:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# wpf_mainwindow_template
---
+++ wpf_mainwindow_template/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+
+namespace wpf_mainwindow_template
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            SetCaptionHeight();
+        }
+
+        private void TitleBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            SetCaptionHeight();
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Maximized;
+        }
+
+        private void Restore_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Normal;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SetCaptionHeight()
+        {
+            switch (WindowState)
+            {
+                case WindowState.Normal:
+                    Chrome.CaptionHeight = TitleBar.ActualHeight + BorderThickness.Top - Chrome.ResizeBorderThickness.Top;
+                    break;
+
+                case WindowState.Maximized:
+                    Chrome.CaptionHeight = TitleBar.ActualHeight - BorderThickness.Top;
+                    break;
+            }
+        }
+    }
+}
